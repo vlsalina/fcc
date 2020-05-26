8c792e033ed4a76c7424ad9a1deeb2ad371fe293
Vincent Salinas <vlsalina@ucsd.edu>
2020-05-26 04:09:49 -0700

    Random quote function working.

--- fcc-front-end-libraries/random-quote-machine/src/App.js
+++ fcc-front-end-libraries/random-quote-machine/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { quotes } from './quotes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idx: 0,
+      bgColor: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let len = quotes.length;
+    let id = Math.floor(Math.random() * len); 
+    this.setState({
+      idx: id
+    })
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+
+          <div className="quote">
+          <h1>"{quotes[this.state.idx].quote}"</h1>
+          </div>
+
+          <div className="author">
+          <h4>--{quotes[this.state.idx].author}</h4>
+          </div>
+
+          <button onClick={() => this.handleClick()}>New Quote</button>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
