f571ee7c60f7f5833f8d34cd22e92ea400ee0b5c
Vincent Salinas <vlsalina@ucsd.edu>
2020-06-26 12:48:30 -0700

    First commit for drum machine

--- fcc-front-end-libraries/drum_machine/src/App.js
+++ fcc-front-end-libraries/drum_machine/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import sounds from "./sounds";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="demo"></div>
+        <div id="drum-machine">
+          <div id="buttons" className="justify-left">
+            <div className="row">
+              <input
+                id="Q"
+                type="button"
+                onKeyPress={this.handleKeyPress}
+              />
+             
+              <button id="W" type="button">
+                W
+              </button>
+              <button id="E" type="button">
+                E
+              </button>
+            </div>
+
+            <div className="row">
+              <button id="A" type="button">
+                A
+              </button>
+              <button id="S" type="button">
+                S
+              </button>
+              <button id="D" type="button">
+                D
+              </button>
+            </div>
+
+            <div className="row">
+              <button id="Z" type="button">
+                Z
+              </button>
+              <button id="X" type="button">
+                X
+              </button>
+              <button id="C" type="button">
+                C
+              </button>
+            </div>
+          </div>
+
+          <div id="controls" className="justify-left" />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
