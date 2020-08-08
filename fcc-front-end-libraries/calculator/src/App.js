b45144f983b2b4aee64f3dbd4c87378d3cdea1ca
Vincent Salinas <vlsalina@ucsd.edu>
2020-08-07 17:52:38 -0700

    Implemented button transition on press functionality.

--- fcc-front-end-libraries/calculator/src/App.js
+++ fcc-front-end-libraries/calculator/src/App.js
@@ -28,8 +28,10 @@ class App extends React.Component {
     this.result = -1.0; // result
     this.op = -1; // 0 = add, 1 = subtract, 2 = multiply, 3 = divide
 
+    // functions
     this.handleClick = this.handleClick.bind(this);
     this.reset = this.reset.bind(this);
+    this.press = this.press.bind(this);
   }
 
   reset() {
@@ -46,6 +48,16 @@ class App extends React.Component {
     this.decimal = false;
     this.dash_token = false;
     this.num_token = false;
+  }
+
+  press(param) {
+    const key =  document.getElementById(param);
+    if (!key) return;
+    key.classList.add("pressed");
+
+    setTimeout(function() {
+      key.classList.remove("pressed");
+    }, 100);
   }
 
   handleClick(e) {
@@ -161,55 +173,55 @@ class App extends React.Component {
           </div>
         </div>
         <div className="buttons">
-          <button className="item1" onClick={() => this.reset()}>
+          <button id="item1" className="item1" onClick={() => {this.reset(); this.press("item1");}}>
             AC
           </button>
-          <button className="item2" onClick={() => this.handleClick(" + ")}>
+          <button id="item2" className="item2" onClick={() => {this.handleClick(" + "); this.press("item2");}}>
             +
           </button>
-          <button className="item3" onClick={() => this.handleClick(" - ")}>
+          <button id="item3" className="item3" onClick={() => {this.handleClick(" - "); this.press("item3");}}>
             -
           </button>
-          <button className="item4" onClick={() => this.handleClick(" / ")}>
+          <button id="item4" className="item4" onClick={() => {this.handleClick(" / "); this.press("item4");}}>
             /
           </button>
-          <button className="item5" onClick={() => this.handleClick(" * ")}>
+          <button id="item5" className="item5" onClick={() => {this.handleClick(" * "); this.press("item5");}}>
             x
           </button>
-          <button className="item6" onClick={() => this.handleClick(" = ")}>
+          <button id="item6" className="item6" onClick={() => {this.handleClick(" = "); this.press("item6");}}>
             =
           </button>
-          <button className="item7" onClick={() => this.handleClick("1")}>
+          <button id="item7" className="item7" onClick={() => {this.handleClick("1"); this.press("item7");}}>
             1
           </button>
-          <button className="item8" onClick={() => this.handleClick("2")}>
+          <button id="item8" className="item8" onClick={() => {this.handleClick("2"); this.press("item8");}}>
             2
           </button>
-          <button className="item9" onClick={() => this.handleClick("3")}>
+          <button id="item9" className="item9" onClick={() => {this.handleClick("3"); this.press("item9");}}>
             3
           </button>
-          <button className="item10" onClick={() => this.handleClick("4")}>
+          <button id="item10" className="item10" onClick={() => {this.handleClick("4"); this.press("item10");}}>
             4
           </button>
-          <button className="item11" onClick={() => this.handleClick("5")}>
+          <button id="item11" className="item11" onClick={() => {this.handleClick("5"); this.press("item11");}}>
             5
           </button>
-          <button className="item12" onClick={() => this.handleClick("6")}>
+          <button id="item12" className="item12" onClick={() => {this.handleClick("6"); this.press("item12");}}>
             6
           </button>
-          <button className="item13" onClick={() => this.handleClick("7")}>
+          <button id="item13" className="item13" onClick={() => {this.handleClick("7"); this.press("item13");}}>
             7
           </button>
-          <button className="item14" onClick={() => this.handleClick("8")}>
+          <button id="item14" className="item14" onClick={() => {this.handleClick("8"); this.press("item14");}}>
             8
           </button>
-          <button className="item15" onClick={() => this.handleClick("9")}>
+          <button id="item15" className="item15" onClick={() => {this.handleClick("9"); this.press("item15");}}>
             9
           </button>
-          <button className="item16" onClick={() => this.handleClick("0")}>
+          <button id="item16" className="item16" onClick={() => {this.handleClick("0"); this.press("item16");}}>
             0
           </button>
-          <button className="item17" onClick={() => this.handleClick(".")}>
+          <button id="item17" className="item17" onClick={() => {this.handleClick("."); this.press("item17");}}>
             .
           </button>
         </div>
