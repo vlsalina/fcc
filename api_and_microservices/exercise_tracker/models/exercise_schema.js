64b7aff790d85f4edf23d9fe7c056ae496109a02
Vincent Salinas <vlsalina@ucsd.edu>
2021-01-20 00:59:35 -0800

    Implemented desired-date(s) feature to log functionality

--- api_and_microservices/exercise_tracker/models/exercise_schema.js
+++ api_and_microservices/exercise_tracker/models/exercise_schema.js
@@ -4,7 +4,7 @@ const exercise_schema = new mongoose.Schema({
   userId: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
-  date: { type: String, required: true }
+  date: { type: Date, required: true }
 }, { collection: 'exercise_data' });
 
 const Model = mongoose.model('Exercise', exercise_schema);
